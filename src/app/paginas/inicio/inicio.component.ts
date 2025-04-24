24031a1a260acdbdaf615c0d43403a9d91a0d5ba
Lichinni <lichisecundario@gmail.com>
2025-04-24 10:59:56 -0300

    creacion de servicio

--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -11,7 +11,7 @@ export class InicioComponent {
   producto = [
     { nombre: "gato cara e tonto", precio: 100, info: "esto es un gato tontin", img: "https://thumbs.dreamstime.com/z/mentira-del-gato-39794060.jpg" },
     { nombre: "otro gatotonto", precio: 150, info: "otro gato muy tontin", img: "https://thumbs.dreamstime.com/z/gato-tonto-75110971.jpg" },
-    { nombre: "le grand chatonton", precio: 200, info: "un francais chat", img: "https://i.redd.it/j1ub9qzg1bb91.png" }
+    { nombre: "le chat", precio: 200, info: "un francais chat", img: "https://i.redd.it/j1ub9qzg1bb91.png" }
   ]
 
   usuario = {
